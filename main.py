b3d8aba18d9b5037c003669bd5896ee27861206a
RatriIsmawati96 <56153676+ratriismawati96@users.noreply.github.com>
2019-12-06 17:34:33 +0700

    requests

---
+++ main.py
@@ -0,0 +1,33 @@
+author = 'Ratri Ismawati'
+email = 'ratriismawati46@gmail.com'
+app_title = 'Menggunakan Python Request, Untuk Memanggil Django API'
+print(f'{app_title} oleh {author}')
+
+#memanggil Api di server Django
+url = 'http://127.0.0.1:8000/'
+import requests
+
+response = requests.get(url)
+if response.status_code == 200:
+    print('koneksi berhasil')
+    #panggil API untuk stats: suhu, humidity dan temperature
+    url_api = f'{url}api/v1/stats/'
+    response = requests.get(url_api)
+    if response.status_code == 200:
+
+
+
+
+        # ubah json ke python dict
+        import json
+        data = json.loads(response.text)
+
+        # data terakhir adalah status sensor terakhir.
+        data_terakhir = data[len(data) - 1]
+        suhu = data_terakhir['temperature']
+        humidity = data_terakhir['humidity']
+        print(f'Hasil pembacaan sensor: suhu={suhu}, humidity = {humidity}')
+
+
+
+
